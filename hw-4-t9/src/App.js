666b5e09dd70ef245a4f65abee1ee2753fb7850f
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-04 03:03:36 +0300

    first commit

---
+++ hw-4-t9/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const langs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+  return (
+    <div>
+      {langs.map(item => <span>{ item + 3 }</span>)}
+    </div>
+  );
+}
+
+export default App;
